c0bf3c73b0ffc1d5833ccbff2f4865185d98a0f1
zurajm <miha@zuraj.com>
2020-01-18 12:39:39 +0100

    Added src

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 VERSION=$(cat VERSION)
 DOCKER_BUILD=zurajm/hass-configurator-docker
-pushd src
-git pull
-popd
+rm -rf ./src/
+git clone https://github.com/danielperna84/hass-configurator.git ./src
 sed -i 's/GIT = False/GIT = True/g' ./src/configurator.py
 docker buildx build --platform linux/amd64,linux/arm64 --push -t ${DOCKER_BUILD} -t ${DOCKER_BUILD}:${VERSION} .
